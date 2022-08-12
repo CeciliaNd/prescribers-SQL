31c62fa774583ee40219b67bf3c151b998cf1833
CeciliaNd <Ceciliakwizer@gmail.com>
2022-08-11 21:09:04 -0500

    1b

---
+++ data/scripts.sql
@@ -0,0 +1,15 @@
+
+-- SELECT *
+--FROM prescriber
+----1a
+/* SELECT npi, SUM(npi)
+FROM prescriber
+GROUP BY npi */
+
+---1b
+SELECT nppes_provider_first_name, nppes_provider_last_org_name, specialty_description
+FROM prescriber
+
+
+
+
