37fb6eb3de16f21af8bd8ec197f1e59579fee06d
CeciliaNd <Ceciliakwizer@gmail.com>
2022-08-13 08:56:26 -0500

    1b

--- data/scripts.sql
+++ data/scripts.sql
@@ -1,14 +1,33 @@
 
--- SELECT *
+--SELECT *
 --FROM prescriber
-----1a
-/* SELECT npi, SUM(npi)
-FROM prescriber
-GROUP BY npi */
+
+-----1a
+/*SELECT max(total_claim_count) AS total_claim_count, max(npi) AS npi
+FROM prescription
+Answer : Total_claim_count:4538 NPI:1992999791*/
 
 ---1b
-SELECT nppes_provider_first_name, nppes_provider_last_org_name, specialty_description
-FROM prescriber
+SELECT max(total_claim_count) AS total_claim_count, max(npi) AS npi nppes_provider_first_name AS first_name, nppes_provider_last_org_name AS last_name, specialty_description
+FROM prescription
+INNER JOIN prescriber
+ON npi.prescriber = npi.prescription;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
